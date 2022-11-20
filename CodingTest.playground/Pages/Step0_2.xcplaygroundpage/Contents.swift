6229c7019df42d9a861bb9a488af802d6a12ecce
jiyeonchoi <choi02647@naver.com>
2022-11-20 23:36:00 +0900

    문자열 뒤집기

--- CodingTest.playground/Pages/Step0_2.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_2.xcplaygroundpage/Contents.swift
@@ -1,5 +1,13 @@
 import Foundation
 
+/* 문자열 뒤집기
+
+ - 문자열 my_string이 매개변수로 주어집니다. my_string을 거꾸로 뒤집은 문자열을 return하도록 solution 함수를 완성해주세요.
+*/
+func solution4(_ my_string:String) -> String { String(my_string.reversed()) }
+
+solution4("jaron")
+
 /* 배열 뒤집기
 
  - 정수가 들어 있는 배열 num_list가 매개변수로 주어집니다. num_list의 원소의 순서를 거꾸로 뒤집은 배열을 return하도록 solution 함수를 완성해주세요.
