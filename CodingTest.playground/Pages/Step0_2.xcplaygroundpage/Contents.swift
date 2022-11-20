ad2e3d0c75de57312686f89cb0f5fcb81b3444d3
jiyeonchoi <choi02647@naver.com>
2022-11-21 01:19:42 +0900

    외계행성의 나이

--- CodingTest.playground/Pages/Step0_2.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_2.xcplaygroundpage/Contents.swift
@@ -1,5 +1,28 @@
 import Foundation
 
+/* 외계행성의 나이
+
+ - 우주여행을 하던 머쓱이는 엔진 고장으로 PROGRAMMERS-962 행성에 불시착하게 됐습니다. 입국심사에서 나이를 말해야 하는데, PROGRAMMERS-962 행성에서는 나이를 알파벳으로 말하고 있습니다. a는 0, b는 1, c는 2, ..., j는 9입니다. 예를 들어 23살은 cd, 51살은 fb로 표현합니다. 나이 age가 매개변수로 주어질 때 PROGRAMMER-962식 나이를 return하도록 solution 함수를 완성해주세요.
+*/
+func solution18(_ age:Int) -> String {
+    let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    return String(age).map{alphabet[Int(String($0))!]}.joined()
+}
+
+func solution17(_ age:Int) -> String {
+    let alphabet:String = "abcdefghij"
+    var result:String = ""
+    var num:Int = age
+    while num>0 {
+        var index:Int = num%10
+        num = num/10
+        result = String(alphabet[alphabet.index(alphabet.startIndex, offsetBy: index)]) + result
+    }
+    return result
+}
+
+solution17(23)
+
 /* 배열 자르기
 
  - 정수 배열 numbers와 정수 num1, num2가 매개변수로 주어질 때, numbers의 num1번 째 인덱스부터 num2번째 인덱스까지 자른 정수 배열을 return 하도록 solution 함수를 완성해보세요.
