ff5f1dcd1c6fbb7b335919bf83643b316b6c692d
jiyeonchoi <choi02647@naver.com>
2022-11-20 14:30:44 +0900

    옷가게 할인 받기

--- CodingTest.playground/Pages/Step0_1.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_1.xcplaygroundpage/Contents.swift
@@ -1,5 +1,17 @@
 import Foundation
 
+/* 옷가게 할인 받기
+
+ - 머쓱이네 옷가게는 10만 원 이상 사면 5%, 30만 원 이상 사면 10%, 50만 원 이상 사면 20%를 할인해줍니다.
+ 구매한 옷의 가격 price가 주어질 때, 지불해야 할 금액을 return 하도록 solution 함수를 완성해보세요.
+*/
+func solution21(_ price:Int) -> Int {
+    var sale: Int = price >= 500000 ? 20 : price >= 300000 ? 10 : price >= 100000 ? 5 : 0
+    return price * (100-sale) / 100
+}
+
+solution21(100000)
+
 /* 배열의 평균값
 
  - 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
