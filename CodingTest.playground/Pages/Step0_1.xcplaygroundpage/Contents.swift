5d817e046da3be01c03243efccd92a908a5ac225
jiyeonchoi <choi02647@naver.com>
2022-11-20 14:35:09 +0900

    아이스 아메리카노

--- CodingTest.playground/Pages/Step0_1.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_1.xcplaygroundpage/Contents.swift
@@ -1,5 +1,17 @@
 import Foundation
 
+/* 아이스 아메리카노
+
+ - 머쓱이는 추운 날에도 아이스 아메리카노만 마십니다. 아이스 아메리카노는 한잔에 5,500원입니다. 머쓱이가 가지고 있는 돈 money가 매개변수로 주어질 때, 머쓱이가 최대로 마실 수 있는 아메리카노의 잔 수와 남는 돈을 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+*/
+func solution23(_ money: Int) -> [Int] { [money / 5500, money % 5500] }
+
+func solution22(_ money:Int) -> [Int] {
+    return [money / 5500, money - (money / 5500) * 5500]
+}
+
+solution21(15000)
+
 /* 옷가게 할인 받기
 
  - 머쓱이네 옷가게는 10만 원 이상 사면 5%, 30만 원 이상 사면 10%, 50만 원 이상 사면 20%를 할인해줍니다.
