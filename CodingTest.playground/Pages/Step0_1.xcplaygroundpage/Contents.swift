a8e01c83e488fa402900e9f8ab8fd3d34d9e3625
jiyeonchoi <choi02647@naver.com>
2022-11-20 14:25:41 +0900

    배열의 평균값

--- CodingTest.playground/Pages/Step0_1.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_1.xcplaygroundpage/Contents.swift
@@ -1,10 +1,25 @@
 import Foundation
 
+/* 배열의 평균값
+
+ - 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+*/
+func solution20(_ numbers: [Int]) -> Double { Double(numbers.reduce(0, +)) / Double(numbers.count) }
+
+func solution19(_ numbers:[Int]) -> Double {
+    var sum: Int = 0
+    for num in numbers {
+        sum += num
+    }
+    return Double(sum) / Double(numbers.count)
+}
+
+solution19([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
 /* 피자 나눠 먹기 (3)
 
  - 머쓱이네 피자가게는 피자를 두 조각에서 열 조각까지 원하는 조각 수로 잘라줍니다. 피자 조각 수 slice와 피자를 먹는 사람의 수 n이 매개변수로 주어질 때, n명의 사람이 최소 한 조각 이상 피자를 먹으려면 최소 몇 판의 피자를 시켜야 하는지를 return 하도록 solution 함수를 완성해보세요.
 */
-
 func solution18(_ slice:Int, _ n:Int) -> Int { n % slice == 0 ? n/slice : (n/slice)+1 }
 
 /* 피자 나눠 먹기 (2)
