f364d6e457a0209991f3927e4a12348309d2df59
jiyeonchoi <choi02647@naver.com>
2022-11-20 23:23:47 +0900

    나이 출력

--- CodingTest.playground/Pages/Step0_2.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_2.xcplaygroundpage/Contents.swift
@@ -1,7 +1,7 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+/* 나이 출력
 
-//: [Next](@next)
+ - 머쓱이는 40살인 선생님이 몇 년도에 태어났는지 궁금해졌습니다. 나이 age가 주어질 때, 2022년을 기준 출생 연도를 return 하는 solution 함수를 완성해주세요.
+*/
+func solution(_ age:Int) -> Int { 2022-age+1 }
