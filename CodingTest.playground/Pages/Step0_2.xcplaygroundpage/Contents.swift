8e0b7cdf73d11eb81dca48571b5c9a2752dc1d90
jiyeonchoi <choi02647@naver.com>
2022-11-21 00:57:06 +0900

    배열 자르기

--- CodingTest.playground/Pages/Step0_2.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_2.xcplaygroundpage/Contents.swift
@@ -1,5 +1,20 @@
 import Foundation
 
+/* 배열 자르기
+
+ - 정수 배열 numbers와 정수 num1, num2가 매개변수로 주어질 때, numbers의 num1번 째 인덱스부터 num2번째 인덱스까지 자른 정수 배열을 return 하도록 solution 함수를 완성해보세요.
+*/
+func solution16(_ numbers:[Int], _ num1:Int, _ num2:Int) -> [Int] {
+    return (num1...num2).map{numbers[$0]}
+}
+
+func solution15(_ numbers:[Int], _ num1:Int, _ num2:Int) -> [Int] {
+    guard numbers.count > num1 && numbers.count > num2 && num2 >= num1 else { return [] }
+    return Array(numbers[num1...num2])
+}
+
+solution15([1,2,3,4,5], 0, 2)
+
 /* 짝수의 합
 
  - 정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.
