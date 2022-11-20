8d6470cc812dcf9d1dce6e9c5c0309c61745069e
jiyeonchoi <choi02647@naver.com>
2022-11-21 00:10:02 +0900

    특정 문자 제거하기

--- CodingTest.playground/Pages/Step0_2.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_2.xcplaygroundpage/Contents.swift
@@ -1,5 +1,21 @@
 import Foundation
 
+/* 특정 문자 제거하기
+
+ - 문자열 my_string과 문자 letter이 매개변수로 주어집니다. my_string에서 letter를 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
+*/
+func solution10(_ my_string:String, _ letter:String) -> String {
+    return my_string.replacingOccurrences(of: letter, with: "")
+}
+
+func solution9(_ my_string:String, _ letter:String) -> String {
+    return my_string.filter{String($0) != letter}
+}
+
+func solution8(_ my_string:String, _ letter:String) -> String { my_string.replacingOccurrences(of: letter, with: "", options: NSString.CompareOptions.literal, range: nil) }
+
+solution8("hello", "h")
+
 /* 문자 반복 출력하기
  
  - 문자열 my_string과 정수 n이 매개변수로 주어질 때, my_string에 들어있는 각 문자를 n만큼 반복한 문자열을 return 하도록 solution 함수를 완성해보세요.
