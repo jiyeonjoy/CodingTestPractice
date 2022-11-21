a09b2fbf87197c16d92cae0f110329ab3eeb864c
jiyeonchoi <choi02647@naver.com>
2022-11-21 23:17:39 +0900

    구슬을 나누는 경우의 수

--- CodingTest.playground/Pages/Step0_2.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_2.xcplaygroundpage/Contents.swift
@@ -1,5 +1,23 @@
 import Foundation
 
+/* 구슬을 나누는 경우의 수
+
+ - 머쓱이는 구슬을 친구들에게 나누어주려고 합니다. 구슬은 모두 다르게 생겼습니다. 머쓱이가 갖고 있는 구슬의 개수 balls와 친구들에게 나누어 줄 구슬 개수 share이 매개변수로 주어질 때, balls개의 구슬 중 share개의 구슬을 고르는 가능한 모든 경우의 수를 return 하는 solution 함수를 완성해주세요.
+*/
+func solution27(_ balls:Int, _ share:Int) -> Int {
+    var mother:Int = 1
+    var child:Int = 1
+    for i in 1...share {
+        child *= (balls+1-i)
+        if child%i == 0 {
+            child /= i
+        } else {
+            mother *= i
+        }
+    }
+    return child/mother
+}
+
 /* 가위 바위 보
 
  - 가위는 2 바위는 0 보는 5로 표현합니다. 가위 바위 보를 내는 순서대로 나타낸 문자열 rsp가 매개변수로 주어질 때, rsp에 저장된 가위 바위 보를 모두 이기는 경우를 순서대로 나타낸 문자열을 return하도록 solution 함수를 완성해보세요.
