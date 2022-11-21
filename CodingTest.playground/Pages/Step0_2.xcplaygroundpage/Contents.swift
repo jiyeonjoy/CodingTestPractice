11a744490489de1687970db9a3cfeeba2f1100cd
jiyeonchoi <choi02647@naver.com>
2022-11-21 22:34:17 +0900

    개미 군단

--- CodingTest.playground/Pages/Step0_2.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_2.xcplaygroundpage/Contents.swift
@@ -1,5 +1,20 @@
 import Foundation
 
+/* 개미 군단
+
+ - 개미 군단이 사냥을 나가려고 합니다. 개미군단은 사냥감의 체력에 딱 맞는 병력을 데리고 나가려고 합니다. 장군개미는 5의 공격력을, 병정개미는 3의 공격력을 일개미는 1의 공격력을 가지고 있습니다. 예를 들어 체력 23의 여치를 사냥하려고 할 때, 일개미 23마리를 데리고 가도 되지만, 장군개미 네 마리와 병정개미 한 마리를 데리고 간다면 더 적은 병력으로 사냥할 수 있습니다. 사냥감의 체력 hp가 매개변수로 주어질 때, 사냥감의 체력에 딱 맞게 최소한의 병력을 구성하려면 몇 마리의 개미가 필요한지를 return하도록 solution 함수를 완성해주세요.
+*/
+func solution23(_ hp:Int) -> Int {
+    let five:Int = 5
+    let three:Int = 3
+    let one:Int = 1
+    var result:Int = 0
+    result += hp/five
+    result += (hp%five)/three
+    result += (hp%five)%three
+    return result
+}
+
 /* 순서쌍의 개수
 
  - 순서쌍이란 두 개의 숫자를 순서를 정하여 짝지어 나타낸 쌍으로 (a, b)로 표기합니다. 자연수 n이 매개변수로 주어질 때 두 숫자의 곱이 n인 자연수 순서쌍의 개수를 return하도록 solution 함수를 완성해주세요.
