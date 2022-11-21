30f24e5094227cf4d02f731b74257872d9e6b9ba
jiyeonchoi <choi02647@naver.com>
2022-11-21 22:16:43 +0900

    진료 순서 정하기

--- CodingTest.playground/Pages/Step0_2.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_2.xcplaygroundpage/Contents.swift
@@ -1,5 +1,33 @@
 import Foundation
 
+/* 진료 순서 정하기
+
+ - 외과의사 머쓱이는 응급실에 온 환자의 응급도를 기준으로 진료 순서를 정하려고 합니다. 정수 배열 emergency가 매개변수로 주어질 때 응급도가 높은 순서대로 진료 순서를 정한 배열을 return하도록 solution 함수를 완성해주세요.
+*/
+func solution20(_ emergency:[Int]) -> [Int] {
+    emergency.map {
+        emergency.sorted(by: > ).firstIndex(of: $0)!+1
+    }
+}
+
+func solution19(_ emergency:[Int]) -> [Int] {
+    var dic:Dictionary<Int,Int> = [:]
+    var i = 1
+    emergency.sorted{ $0 > $1 }.map {
+        dic[$0] = i
+        i += 1
+    }
+    var result:[Int] = []
+    for i in emergency {
+        if let num = dic[i] {
+            result.append(num)
+        }
+    }
+    return result
+}
+
+solution20([3, 76, 24])
+
 /* 외계행성의 나이
 
  - 우주여행을 하던 머쓱이는 엔진 고장으로 PROGRAMMERS-962 행성에 불시착하게 됐습니다. 입국심사에서 나이를 말해야 하는데, PROGRAMMERS-962 행성에서는 나이를 알파벳으로 말하고 있습니다. a는 0, b는 1, c는 2, ..., j는 9입니다. 예를 들어 23살은 cd, 51살은 fb로 표현합니다. 나이 age가 매개변수로 주어질 때 PROGRAMMER-962식 나이를 return하도록 solution 함수를 완성해주세요.
