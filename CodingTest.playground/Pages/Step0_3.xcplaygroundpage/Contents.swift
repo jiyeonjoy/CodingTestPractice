0e70029ed7770213f5cad077c9d2257b662be578
jiyeonchoi <choi02647@naver.com>
2022-11-22 22:57:22 +0900

    최댓값 만들기 (1)

--- CodingTest.playground/Pages/Step0_3.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_3.xcplaygroundpage/Contents.swift
@@ -1,5 +1,14 @@
 import Foundation
 
+/* 최댓값 만들기 (1)
+
+ - 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을 return하도록 solution 함수를 완성해주세요.
+*/
+func solution7(_ numbers:[Int]) -> Int {
+    var sorted = numbers.sorted()
+    return sorted[numbers.count-2] * sorted[numbers.count-1]
+}
+
 /* 합성수 찾기
  
  - 약수의 개수가 세 개 이상인 수를 합성수라고 합니다. 자연수 n이 매개변수로 주어질 때 n이하의 합성수의 개수를 return하도록 solution 함수를 완성해주세요.
