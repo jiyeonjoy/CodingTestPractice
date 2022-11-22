9862d393635c947ed63c8f93f0ae279e892a9c5b
jiyeonchoi <choi02647@naver.com>
2022-11-22 22:35:32 +0900

    주사위의 개수

--- CodingTest.playground/Pages/Step0_3.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_3.xcplaygroundpage/Contents.swift
@@ -1,5 +1,22 @@
 import Foundation
 
+/* 주사위의 개수
+ 
+ - 머쓱이는 직육면체 모양의 상자를 하나 가지고 있는데 이 상자에 정육면체 모양의 주사위를 최대한 많이 채우고 싶습니다. 상자의 가로, 세로, 높이가 저장되어있는 배열 box와 주사위 모서리의 길이 정수 n이 매개변수로 주어졌을 때, 상자에 들어갈 수 있는 주사위의 최대 개수를 return 하도록 solution 함수를 완성해주세요.
+*/
+func solution4(_ box:[Int], _ n:Int) -> Int {
+    guard box.count > 2 && box[0] >= n && box[1] >= n && box[2] >= n else {
+        return 0
+    }
+    var result:Int = 1
+    for size in box {
+        result *= size/n
+    }
+    return result
+}
+
+solution4([10,8,6],3)
+
 /* 배열 회전시키기
  
  - 정수가 담긴 배열 numbers와 문자열 direction가 매개변수로 주어집니다. 배열 numbers의 원소를 direction방향으로 한 칸씩 회전시킨 배열을 return하도록 solution 함수를 완성해주세요.
