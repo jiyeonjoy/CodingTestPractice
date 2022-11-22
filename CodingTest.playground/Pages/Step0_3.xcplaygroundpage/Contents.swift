fbc92e658304a65240ab60fc6f992488d648696e
jiyeonchoi <choi02647@naver.com>
2022-11-23 00:04:53 +0900

    소인수분해

--- CodingTest.playground/Pages/Step0_3.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_3.xcplaygroundpage/Contents.swift
@@ -1,5 +1,29 @@
 import Foundation
 
+/* 소인수분해
+
+ - 소인수분해란 어떤 수를 소수들의 곱으로 표현하는 것입니다. 예를 들어 12를 소인수 분해하면 2 * 2 * 3 으로 나타낼 수 있습니다. 따라서 12의 소인수는 2와 3입니다. 자연수 n이 매개변수로 주어질 때 n의 소인수를 오름차순으로 담은 배열을 return하도록 solution 함수를 완성해주세요.
+*/
+func solution18(_ n:Int) -> [Int] {
+    if n == 2 { return [2] }
+    var result:[Int] = []
+    var num = n
+    for i in 2...n {
+        if num%i == 0 {
+            result.append(i)
+            while num%i == 0 {
+                num /= i
+            }
+        }
+        if num == 1 {
+            break
+        }
+    }
+    return result
+}
+
+solution18(420)
+
 /* 숨어있는 숫자의 덧셈 (1)
 
  - 문자열 my_string이 매개변수로 주어집니다. my_string안의 모든 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
