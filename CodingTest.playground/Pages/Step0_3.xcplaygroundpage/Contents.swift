96f1de4f3bdc72ddcb0bdb04659ccb8929208218
jiyeonchoi <choi02647@naver.com>
2022-11-22 23:45:51 +0900

    문자열 정렬하기 (1)

--- CodingTest.playground/Pages/Step0_3.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_3.xcplaygroundpage/Contents.swift
@@ -1,5 +1,21 @@
 import Foundation
 
+/* 문자열 정렬하기 (1)
+
+ - 문자열 my_string이 매개변수로 주어질 때, my_string 안에 있는 숫자만 골라 오름차순 정렬한 리스트를 return 하도록 solution 함수를 작성해보세요.
+*/
+func solution15(_ my_string:String) -> [Int] {
+    return my_string.filter{$0.isNumber}.map{Int(String($0))!}.sorted(by: <)
+}
+
+func solution14(_ my_string:String) -> [Int] { my_string.filter {
+    ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"].contains($0) }.map {
+        Int(String($0)) ?? 0
+    }.sorted()
+}
+
+solution14("1가나23")
+
 /* 모음 제거
 
  - 영어에선 a, e, i, o, u 다섯 가지 알파벳을 모음으로 분류합니다. 문자열 my_string이 매개변수로 주어질 때 모음을 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
