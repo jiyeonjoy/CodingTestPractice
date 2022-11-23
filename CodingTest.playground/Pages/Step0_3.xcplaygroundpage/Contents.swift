45c71e2eb57e8de0cfd7403ae4f574b540cbd613
jiyeonchoi <choi02647@naver.com>
2022-11-23 23:23:55 +0900

    직사각형 넓이 구하기

--- CodingTest.playground/Pages/Step0_3.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_3.xcplaygroundpage/Contents.swift
@@ -3,6 +3,29 @@ import Foundation
 
  -
 */
+/* 직사각형 넓이 구하기
+
+ - 2차원 좌표 평면에 변이 축과 평행한 직사각형이 있습니다. 직사각형 네 꼭짓점의 좌표 [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]가 담겨있는 배열 dots가 매개변수로 주어질 때, 직사각형의 넓이를 return 하도록 solution 함수를 완성해보세요.
+*/
+func solution21(_ dots:[[Int]]) -> Int {
+    guard dots.count < 5 else { return 0 }
+   
+    var width:Int = 1
+    var height:Int = 1
+    
+    for i in 1...3 {
+        if dots[0][0] == dots[i][0] {
+            width = dots[i][1] > dots[0][1] ? dots[i][1] - dots[0][1] : dots[0][1] - dots[i][1]
+        }
+        if dots[0][1] == dots[i][1] {
+            height = dots[i][0] > dots[0][0] ? dots[i][0] - dots[0][0] : dots[0][0] - dots[i][0]
+        }
+    }
+    
+    return width * height
+}
+
+solution21([[-1, -1], [1, 1], [1, -1], [-1, 1]])
 
 /* 배열 원소의 길이
 
