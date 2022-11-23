a96df1427ce2d8fcb644fc4b18ceff54bd5c47cc
jiyeonchoi <choi02647@naver.com>
2022-11-24 01:05:56 +0900

    외계어 사전

--- CodingTest.playground/Pages/Step0_3.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_3.xcplaygroundpage/Contents.swift
@@ -1,8 +1,19 @@
 import Foundation
-/*
 
- -
+/* 외계어 사전
+
+ - PROGRAMMERS-962 행성에 불시착한 우주비행사 머쓱이는 외계행성의 언어를 공부하려고 합니다. 알파벳이 담긴 배열 spell과 외계어 사전 dic이 매개변수로 주어집니다. spell에 담긴 알파벳을 한번씩만 모두 사용한 단어가 dic에 존재한다면 1, 존재하지 않는다면 2를 return하도록 solution 함수를 완성해주세요.
 */
+func solution28(_ spell:[String], _ dic:[String]) -> Int {
+    var isMatch:Bool = dic.filter {
+           $0.count == spell.count
+        && $0.compactMap{ String($0) }.sorted().joined() == spell.sorted().joined()
+    }.count > 0
+       
+    return isMatch ? 1 : 2
+}
+
+solution28(["p", "o", "s"], ["sod", "eocd", "qixm", "adio", "soo"])
 
 /* 삼각형의 완성조건 (2)
 
