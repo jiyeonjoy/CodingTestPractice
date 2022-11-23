641821c34cc6373574610b09b09b5bcc52da8777
jiyeonchoi <choi02647@naver.com>
2022-11-24 00:39:48 +0900

    삼각형의 완성조건 (2)

--- CodingTest.playground/Pages/Step0_3.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_3.xcplaygroundpage/Contents.swift
@@ -3,6 +3,20 @@ import Foundation
 
  -
 */
+
+/* 삼각형의 완성조건 (2)
+
+ - 선분 세 개로 삼각형을 만들기 위해서는 다음과 같은 조건을 만족해야 합니다.
+ 
+ 가장 긴 변의 길이는 다른 두 변의 길이의 합보다 작아야 합니다.
+ 삼각형의 두 변의 길이가 담긴 배열 sides이 매개변수로 주어집니다. 나머지 한 변이 될 수 있는 정수의 개수를 return하도록 solution 함수를 완성해주세요.
+*/
+func solution27(_ sides:[Int]) -> Int {
+    let long:Int = max(sides[0],sides[1])
+    let short:Int = min(sides[0],sides[1])
+    return (long-short+1...long+short-1).count
+}
+
 /* 안전지대
 
  - 다음 그림과 같이 지뢰가 있는 지역과 지뢰에 인접한 위, 아래, 좌, 우 대각선 칸을 모두 위험지역으로 분류합니다.
