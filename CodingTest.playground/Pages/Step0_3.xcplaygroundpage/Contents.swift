8a93edf23a3f42efefe019746f10188996e7f21b
jiyeonchoi <choi02647@naver.com>
2022-11-23 23:30:32 +0900

    캐릭터의 좌표

--- CodingTest.playground/Pages/Step0_3.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_3.xcplaygroundpage/Contents.swift
@@ -3,6 +3,39 @@ import Foundation
 
  -
 */
+/* 캐릭터의 좌표
+
+ - 머쓱이는 RPG게임을 하고 있습니다. 게임에는 up, down, left, right 방향키가 있으며 각 키를 누르면 위, 아래, 왼쪽, 오른쪽으로 한 칸씩 이동합니다. 예를 들어 [0,0]에서 up을 누른다면 캐릭터의 좌표는 [0, 1], down을 누른다면 [0, -1], left를 누른다면 [-1, 0], right를 누른다면 [1, 0]입니다. 머쓱이가 입력한 방향키의 배열 keyinput와 맵의 크기 board이 매개변수로 주어집니다. 캐릭터는 항상 [0,0]에서 시작할 때 키 입력이 모두 끝난 뒤에 캐릭터의 좌표 [x, y]를 return하도록 solution 함수를 완성해주세요.
+ 
+ [0, 0]은 board의 정 중앙에 위치합니다. 예를 들어 board의 가로 크기가 9라면 캐릭터는 왼쪽으로 최대 [-4, 0]까지 오른쪽으로 최대 [4, 0]까지 이동할 수 있습니다.
+*/
+func solution22(_ keyinput:[String], _ board:[Int]) -> [Int] {
+    var x:Int = 0
+    var y:Int = 0
+    
+    for key in keyinput {
+        if key == "up" {
+            if y != board[1]/2 {
+                y += 1
+            }
+        } else if key == "down" {
+            if y != -board[1]/2 {
+                y -= 1
+            }
+        } else if key == "left" {
+            if x != -board[0]/2 {
+                x -= 1
+            }
+        } else {
+            if x != board[0]/2 {
+                x += 1
+            }
+        }
+    }
+    
+    return [x,y]
+}
+
 /* 직사각형 넓이 구하기
 
  - 2차원 좌표 평면에 변이 축과 평행한 직사각형이 있습니다. 직사각형 네 꼭짓점의 좌표 [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]가 담겨있는 배열 dots가 매개변수로 주어질 때, 직사각형의 넓이를 return 하도록 solution 함수를 완성해보세요.
