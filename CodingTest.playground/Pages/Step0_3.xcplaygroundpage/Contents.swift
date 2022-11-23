21b5cb4177d285185c6c41ae1cac339a803d6a9d
jiyeonchoi <choi02647@naver.com>
2022-11-24 00:06:37 +0900

    숨어있는 숫자의 덧셈 (2)

--- CodingTest.playground/Pages/Step0_3.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_3.xcplaygroundpage/Contents.swift
@@ -3,6 +3,20 @@ import Foundation
 
  -
 */
+/* 숨어있는 숫자의 덧셈 (2)
+
+ - 문자열 my_string이 매개변수로 주어집니다. my_string은 소문자, 대문자, 자연수로만 구성되어있습니다. my_string안의 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
+*/
+func solution25(_ my_string:String) -> Int {
+    var result:Int = 0
+    var str:String = my_string.replacingOccurrences(of: "[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ]", with: ".",options: .regularExpression)
+    str.components(separatedBy: ".").map { result += (Int($0) ?? 0) }
+    
+    return result
+}
+
+solution25("aAb1B2cC34oOp")
+
 /* 다항식 더하기
 
  - 한 개 이상의 항의 합으로 이루어진 식을 다항식이라고 합니다. 다항식을 계산할 때는 동류항끼리 계산해 정리합니다. 덧셈으로 이루어진 다항식 polynomial이 매개변수로 주어질 때, 동류항끼리 더한 결괏값을 문자열로 return 하도록 solution 함수를 완성해보세요. 같은 식이라면 가장 짧은 수식을 return 합니다.
