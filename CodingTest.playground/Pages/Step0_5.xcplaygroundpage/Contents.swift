19db7679e0f1ce10e67cec137e6e57570ca2a481
jiyeonchoi <choi02647@naver.com>
2022-11-25 02:37:53 +0900

    잘라서 배열로 저장하기

--- CodingTest.playground/Pages/Step0_5.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_5.xcplaygroundpage/Contents.swift
@@ -21,10 +21,33 @@ import Foundation
 
  -
 */
-/*
 
- -
+/* 잘라서 배열로 저장하기
+
+ - 문자열 my_str과 n이 매개변수로 주어질 때, my_str을 길이 n씩 잘라서 저장한 배열을 return하도록 solution 함수를 완성해주세요.
 */
+extension String {
+    func index(from: Int) -> Index {
+        return self.index(startIndex, offsetBy: from)
+    }
+    
+    func substring(with r: Range<Int>) -> String {
+        let startIndex = index(from: r.lowerBound)
+        let endIndex = index(from: r.upperBound)
+        return String(self[startIndex..<endIndex])
+    }
+}
+
+func solution15(_ my_str:String, _ n:Int) -> [String] {
+    var result:[String] = []
+    for i in 0...(my_str.count-1)/n {
+        result.append(
+            my_str.substring(with: i*n..<min(i*n+n,my_str.count)))
+    }
+    return result
+}
+
+solution15("abc1Addfggg4556b", 6)
 
 /* 7의 개수
 
