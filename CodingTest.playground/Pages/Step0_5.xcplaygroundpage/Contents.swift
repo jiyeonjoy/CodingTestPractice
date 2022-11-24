4e3ed5f6d4a2375cfc4e79045b4a4cf81d023bd9
jiyeonchoi <choi02647@naver.com>
2022-11-25 03:21:07 +0900

    연속된 수의 합

--- CodingTest.playground/Pages/Step0_5.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_5.xcplaygroundpage/Contents.swift
@@ -9,10 +9,24 @@ import Foundation
 
  -
 */
-/*
 
- -
+/* 연속된 수의 합
+
+ - 연속된 세 개의 정수를 더해 12가 되는 경우는 3, 4, 5입니다. 두 정수 num과 total이 주어집니다. 연속된 수 num개를 더한 값이 total이 될 때, 정수 배열을 오름차순으로 담아 return하도록 solution함수를 완성해보세요.
 */
+func solution18(_ num:Int, _ total:Int) -> [Int] {
+    var result:[Int] = []
+    if num%2 == 0 {
+        for i in 0..<num {
+            result.append((total/num)-(num/2)+i+1)
+        }
+    } else {
+        for i in 0..<num {
+            result.append((total/num)-(num/2)+i)
+        }
+    }
+    return result
+}
 
 /* 종이 자르기
 
