663340cd3360f4794990ed60c05e835d20fd4a8e
jiyeonchoi <choi02647@naver.com>
2022-11-24 22:09:05 +0900

    겹치는 선분의 길이

--- CodingTest.playground/Pages/Step0_4.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_4.xcplaygroundpage/Contents.swift
@@ -68,10 +68,30 @@ import Foundation
 
  -
 */
-/*
+/* 겹치는 선분의 길이
 
- -
+ - 빨간색, 초록색, 파란색 선분이 x축 위에 있습니다. 세 선분의 x좌표 시작과 끝이 [[start, end], [start, end], [start, end]] 형태로 들어있는 2차원 배열 lines가 매개변수로 주어질 때, 두 개 이상의 선분이 겹치는 부분의 길이를return 하도록 solution 함수를 완성해보세요.
+ 
+ lines가 [[0, 2], [-3, -1], [-2, 1]]일 때 그림으로 나타내면 다음과 같습니다.
+
+ 선분이 두 개 이상 겹친 곳은 [-2, -1], [0, 1]로 2만큼 겹쳐있습니다.
 */
+func solution4(_ lines:[[Int]]) -> Int {
+    var result = Set<Int>()
+    for i in lines[0][0]+1...lines[0][1] {
+        if (i > lines[1][0] && i <= lines[1][1]) ||
+            (i > lines[2][0] && i <= lines[2][1]) {
+            result.insert(i)
+        }
+    }
+    for i in lines[1][0]+1...lines[1][1] {
+        if (i > lines[2][0] && i <= lines[2][1]) {
+            result.insert(i)
+        }
+    }
+    return result.count
+}
+
 /* 평행
 
  - 점 네 개의 좌표를 담은 이차원 배열  dots가 다음과 같이 매개변수로 주어집니다.
