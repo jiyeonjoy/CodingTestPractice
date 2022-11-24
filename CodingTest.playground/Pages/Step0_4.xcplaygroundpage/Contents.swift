f5ea8fd06ab788d93955957d0bd9a5be4c815b9b
jiyeonchoi <choi02647@naver.com>
2022-11-25 00:42:17 +0900

    영어가 싫어요

--- CodingTest.playground/Pages/Step0_4.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_4.xcplaygroundpage/Contents.swift
@@ -8,10 +8,16 @@ import Foundation
 
  -
 */
-/*
 
- -
+/* 영어가 싫어요
+
+ - 영어가 싫은 머쓱이는 영어로 표기되어있는 숫자를 수로 바꾸려고 합니다. 문자열 numbers가 매개변수로 주어질 때, numbers를 정수로 바꿔 return 하도록 solution 함수를 완성해 주세요.
 */
+func solution21(_ numbers:String) -> Int64 {
+       Int64(numbers.replacingOccurrences(of: "zero", with: "0").replacingOccurrences(of: "one", with: "1").replacingOccurrences(of: "two", with: "2").replacingOccurrences(of: "three", with: "3").replacingOccurrences(of: "four", with: "4").replacingOccurrences(of: "five", with: "5").replacingOccurrences(of: "six", with: "6").replacingOccurrences(of: "seven", with: "7").replacingOccurrences(of: "eight", with: "8").replacingOccurrences(of: "nine", with: "9") ) ?? 0
+}
+
+solution21("onetwothreefourfivesixseveneightnine")
 
 /* 대문자와 소문자
 
