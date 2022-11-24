74f30008f7da779f16083f404aaccde14a1a673d
jiyeonchoi <choi02647@naver.com>
2022-11-25 01:19:55 +0900

    편지

--- CodingTest.playground/Pages/Step0_5.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_5.xcplaygroundpage/Contents.swift
@@ -73,10 +73,14 @@ import Foundation
 
  -
 */
-/*
+/* 편지
 
- -
+ - 머쓱이는 할머니께 생신 축하 편지를 쓰려고 합니다. 할머니가 보시기 편하도록 글자 한 자 한 자를 가로 2cm 크기로 적으려고 하며, 편지를 가로로만 적을 때, 축하 문구 message를 적기 위해 필요한 편지지의 최소 가로길이를 return 하도록 solution 함수를 완성해주세요.
 */
+func solution2(_ message:String) -> Int {
+    message.count*2
+}
+
 /* 약수 구하기
 
  - 정수 n이 매개변수로 주어질 때, n의 약수를 오름차순으로 담은 배열을 return하도록 solution 함수를 완성해주세요.
