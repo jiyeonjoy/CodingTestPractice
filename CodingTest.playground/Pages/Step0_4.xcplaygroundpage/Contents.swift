f4c2f9ef270df10e380d14198e778a7ece38c375
jiyeonchoi <choi02647@naver.com>
2022-11-24 23:04:32 +0900

    로그인 성공?

--- CodingTest.playground/Pages/Step0_4.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_4.xcplaygroundpage/Contents.swift
@@ -52,10 +52,26 @@ import Foundation
 
  -
 */
-/*
+/* 로그인 성공?
 
- -
+ - 머쓱이는 프로그래머스에 로그인하려고 합니다. 머쓱이가 입력한 아이디와 패스워드가 담긴 배열 id_pw와 회원들의 정보가 담긴 2차원 배열 db가 주어질 때, 다음과 같이 로그인 성공, 실패에 따른 메시지를 return하도록 solution 함수를 완성해주세요.
+ 
+ 아이디와 비밀번호가 모두 일치하는 회원정보가 있으면 "login"을 return합니다.
+ 로그인이 실패했을 때 아이디가 일치하는 회원이 없다면 “fail”를, 아이디는 일치하지만 비밀번호가 일치하는 회원이 없다면 “wrong pw”를 return 합니다.
 */
+func solution9(_ id_pw:[String], _ db:[[String]]) -> String {
+    for data in db {
+        if id_pw[0] == data[0] {
+            if id_pw[1] == data[1] {
+                return "login"
+            } else {
+                return "wrong pw"
+            }
+        }
+    }
+    return "fail"
+}
+
 /* 등수 매기기
 
  - 영어 점수와 수학 점수의 평균 점수를 기준으로 학생들의 등수를 매기려고 합니다. 영어 점수와 수학 점수를 담은 2차원 정수 배열 score가 주어질 때, 영어 점수와 수학 점수의 평균을 기준으로 매긴 등수를 담은 배열을 return하도록 solution 함수를 완성해주세요.
