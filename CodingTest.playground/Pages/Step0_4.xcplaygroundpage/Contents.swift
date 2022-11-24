3ec5fd51d35983dffce3e9913eb0749b713b9889
jiyeonchoi <choi02647@naver.com>
2022-11-24 23:37:43 +0900

    A로 B 만들기

--- CodingTest.playground/Pages/Step0_4.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_4.xcplaygroundpage/Contents.swift
@@ -45,6 +45,8 @@ import Foundation
 
  - 문자열 before와 after가 매개변수로 주어질 때, before의 순서를 바꾸어 after를 만들 수 있으면 1을, 만들 수 없으면 0을 return 하도록 solution 함수를 완성해보세요.
 */
+func solution13(_ before: String, _ after: String) -> Int { Array(before).sorted() == Array(after).sorted() ? 1 : 0 }
+
 func solution12(_ before:String, _ after:String) -> Int {
     before.compactMap{ String($0) }.sorted().joined()
     == after.compactMap{ String($0) }.sorted().joined() ? 1 : 0
