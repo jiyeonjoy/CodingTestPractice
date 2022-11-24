a70f896da7ae9ca8a94bfb43d5e151e89c29031c
jiyeonchoi <choi02647@naver.com>
2022-11-24 23:31:46 +0900

    이진수 더하기

--- CodingTest.playground/Pages/Step0_4.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_4.xcplaygroundpage/Contents.swift
@@ -44,10 +44,17 @@ import Foundation
 
  -
 */
-/*
 
- -
+/* 이진수 더하기
+
+ - 이진수를 의미하는 두 개의 문자열 bin1과 bin2가 매개변수로 주어질 때, 두 이진수의 합을 return하도록 solution 함수를 완성해주세요.
 */
+func solution11(_ bin1:String, _ bin2:String) -> String {
+    String(Int(bin1, radix: 2)! + Int(bin2, radix: 2)!, radix: 2)
+}
+
+solution11("10", "11")
+
 /* 치킨 쿠폰
 
  - 프로그래머스 치킨은 치킨을 시켜먹으면 한 마리당 쿠폰을 한 장 발급합니다. 쿠폰을 열 장 모으면 치킨을 한 마리 서비스로 받을 수 있고, 서비스 치킨에도 쿠폰이 발급됩니다. 시켜먹은 치킨의 수 chicken이 매개변수로 주어질 때 받을 수 있는 최대 서비스 치킨의 수를 return하도록 solution 함수를 완성해주세요.
