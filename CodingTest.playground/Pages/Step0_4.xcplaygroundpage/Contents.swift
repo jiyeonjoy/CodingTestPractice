7b9719064fbda691d61321479a23a8c44b8b27f0
jiyeonchoi <choi02647@naver.com>
2022-11-24 23:47:56 +0900

    중복된 문자 제거

--- CodingTest.playground/Pages/Step0_4.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_4.xcplaygroundpage/Contents.swift
@@ -32,10 +32,22 @@ import Foundation
 
  -
 */
-/*
 
- -
+/* 중복된 문자 제거
+
+ - 문자열 my_string이 매개변수로 주어집니다. my_string에서 중복된 문자를 제거하고 하나의 문자만 남긴 문자열을 return하도록 solution 함수를 완성해주세요.
 */
+func solution15(_ my_string:String) -> String {
+    var result:String = ""
+    my_string.map {
+        if !result.contains($0) {
+            result += String($0)
+        }
+    }
+    return result
+}
+
+solution15("people")
 
 /* k의 개수
 
