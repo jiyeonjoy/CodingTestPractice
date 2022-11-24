e6515acf50bf75dc979e5f107935248058d6f6ca
jiyeonchoi <choi02647@naver.com>
2022-11-25 00:33:01 +0900

    대문자와 소문자

--- CodingTest.playground/Pages/Step0_4.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_4.xcplaygroundpage/Contents.swift
@@ -12,10 +12,22 @@ import Foundation
 
  -
 */
-/*
 
- -
+/* 대문자와 소문자
+
+ - 문자열 my_string이 매개변수로 주어질 때, 대문자는 소문자로 소문자는 대문자로 변환한 문자열을 return하도록 solution 함수를 완성해주세요.
 */
+func solution20(_ my_string:String) -> String {
+    var result:String = ""
+    my_string.map {
+        if ($0.asciiValue ?? 0) > 64 && ($0.asciiValue ?? 0) < 91 {
+            result += String($0).lowercased()
+        } else {
+            result += String($0).uppercased()
+        }
+    }
+    return result
+}
 
 /* 암호 해독
 
