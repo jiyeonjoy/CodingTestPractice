0f8883cf43160f3d15c954496d4118b6b9467c06
jiyeonchoi <choi02647@naver.com>
2022-11-25 02:06:51 +0900

    제곱수 판별하기

--- CodingTest.playground/Pages/Step0_5.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_5.xcplaygroundpage/Contents.swift
@@ -37,10 +37,20 @@ import Foundation
 
  -
 */
-/*
+/* 제곱수 판별하기
 
- -
+ - 어떤 자연수를 제곱했을 때 나오는 정수를 제곱수라고 합니다. 정수 n이 매개변수로 주어질 때, n이 제곱수라면 1을 아니라면 2를 return하도록 solution 함수를 완성해주세요.
 */
+func solution11(_ n:Int) -> Int {
+    var count:Int = 1
+    while count < n {
+        if count*count == n {
+            return 1
+        }
+        count += 1
+    }
+    return 2
+}
 
 /* 문자열안에 문자열
 
