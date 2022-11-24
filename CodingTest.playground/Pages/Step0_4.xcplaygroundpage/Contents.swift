5685dc0a96c74feaa3c714d1c14de395dd4e8720
jiyeonchoi <choi02647@naver.com>
2022-11-25 00:08:29 +0900

    암호 해독

--- CodingTest.playground/Pages/Step0_4.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_4.xcplaygroundpage/Contents.swift
@@ -16,10 +16,25 @@ import Foundation
 
  -
 */
-/*
 
- -
+/* 암호 해독
+
+ - 군 전략가 머쓱이는 전쟁 중 적군이 다음과 같은 암호 체계를 사용한다는 것을 알아냈습니다.
+ 
+ 암호화된 문자열 cipher를 주고받습니다.
+ 그 문자열에서 code의 배수 번째 글자만 진짜 암호입니다.
+ 문자열 cipher와 정수 code가 매개변수로 주어질 때 해독된 암호 문자열을 return하도록 solution 함수를 완성해주세요.
 */
+func solution19(_ cipher:String, _ code:Int) -> String {
+    var result:String = ""
+    var list = Array(cipher)
+    for i in 1...cipher.count {
+        if i%code == 0 {
+           result += String(list[i-1])
+        }
+    }
+    return result
+}
 
 /* 369게임
 
