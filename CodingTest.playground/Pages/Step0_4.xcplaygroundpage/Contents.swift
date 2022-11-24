db07492e84882bd73d6ed485f794f735bb9829f2
jiyeonchoi <choi02647@naver.com>
2022-11-24 23:16:15 +0900

    치킨 쿠폰

--- CodingTest.playground/Pages/Step0_4.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_4.xcplaygroundpage/Contents.swift
@@ -48,10 +48,22 @@ import Foundation
 
  -
 */
-/*
+/* 치킨 쿠폰
 
- -
+ - 프로그래머스 치킨은 치킨을 시켜먹으면 한 마리당 쿠폰을 한 장 발급합니다. 쿠폰을 열 장 모으면 치킨을 한 마리 서비스로 받을 수 있고, 서비스 치킨에도 쿠폰이 발급됩니다. 시켜먹은 치킨의 수 chicken이 매개변수로 주어질 때 받을 수 있는 최대 서비스 치킨의 수를 return하도록 solution 함수를 완성해주세요.
 */
+func solution10(_ chicken:Int) -> Int {
+    var chickenCount:Int = chicken
+    var service:Int = 0
+    while chickenCount/10 > 0 {
+        service += chickenCount/10
+        chickenCount = chickenCount/10 + chickenCount%10
+    }
+    return service
+}
+
+solution10(1081)
+
 /* 로그인 성공?
 
  - 머쓱이는 프로그래머스에 로그인하려고 합니다. 머쓱이가 입력한 아이디와 패스워드가 담긴 배열 id_pw와 회원들의 정보가 담긴 2차원 배열 db가 주어질 때, 다음과 같이 로그인 성공, 실패에 따른 메시지를 return하도록 solution 함수를 완성해주세요.
