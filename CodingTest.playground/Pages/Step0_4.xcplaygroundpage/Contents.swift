6e0c60e99f27c4aad65e5bf6b51d57fa5eece3e4
jiyeonchoi <choi02647@naver.com>
2022-11-24 22:55:30 +0900

    등수 매기기

--- CodingTest.playground/Pages/Step0_4.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_4.xcplaygroundpage/Contents.swift
@@ -56,10 +56,20 @@ import Foundation
 
  -
 */
-/*
+/* 등수 매기기
 
- -
+ - 영어 점수와 수학 점수의 평균 점수를 기준으로 학생들의 등수를 매기려고 합니다. 영어 점수와 수학 점수를 담은 2차원 정수 배열 score가 주어질 때, 영어 점수와 수학 점수의 평균을 기준으로 매긴 등수를 담은 배열을 return하도록 solution 함수를 완성해주세요.
 */
+func solution7(_ score:[[Int]]) -> [Int] {
+    var list:[Int] = score.map{ ($0[0]+$0[1])/2 }
+    var sorted:[Int] = list.sorted(by: >)
+    return list.map {
+        (sorted.firstIndex(of:$0) ?? 0)+1
+    }
+}
+
+solution7([[80, 70], [90, 50], [40, 70], [50, 80]])
+
 /* 특이한 정렬
 
  - 정수 n을 기준으로 n과 가까운 수부터 정렬하려고 합니다. 이때 n으로부터의 거리가 같다면 더 큰 수를 앞에 오도록 배치합니다. 정수가 담긴 배열 numlist와 정수 n이 주어질 때 numlist의 원소를 n으로부터 가까운 순서대로 정렬한 배열을 return하도록 solution 함수를 완성해주세요.
