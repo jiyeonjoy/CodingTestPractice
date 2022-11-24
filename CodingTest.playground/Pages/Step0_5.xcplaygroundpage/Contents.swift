c4e55d0f786eb607d7edb2ee0896da1d926cadc5
jiyeonchoi <choi02647@naver.com>
2022-11-25 02:13:50 +0900

    세균 증식

--- CodingTest.playground/Pages/Step0_5.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_5.xcplaygroundpage/Contents.swift
@@ -33,10 +33,21 @@ import Foundation
 
  -
 */
-/*
 
- -
+/* 세균 증식
+
+ - 어떤 세균은 1시간에 두배만큼 증식한다고 합니다. 처음 세균의 마리수 n과 경과한 시간 t가 매개변수로 주어질 때 t시간 후 세균의 수를 return하도록 solution 함수를 완성해주세요.
 */
+func solution12(_ n:Int, _ t:Int) -> Int {
+    var result:Int = n
+    for _ in 1...t {
+        result *= 2
+    }
+    return result
+}
+
+solution12(2, 10)
+
 /* 제곱수 판별하기
 
  - 어떤 자연수를 제곱했을 때 나오는 정수를 제곱수라고 합니다. 정수 n이 매개변수로 주어질 때, n이 제곱수라면 1을 아니라면 2를 return하도록 solution 함수를 완성해주세요.
