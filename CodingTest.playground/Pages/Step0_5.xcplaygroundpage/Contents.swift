99a8e4ff84ce279a79e88f1291bdbb32c3659167
jiyeonchoi <choi02647@naver.com>
2022-11-25 03:25:53 +0900

    다음에 올 숫자

--- CodingTest.playground/Pages/Step0_5.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_5.xcplaygroundpage/Contents.swift
@@ -5,10 +5,17 @@ import Foundation
  -
 */
 
-/*
+/* 다음에 올 숫자
 
- -
+ - 등차수열 혹은 등비수열 common이 매개변수로 주어질 때, 마지막 원소 다음으로 올 숫자를 return 하도록 solution 함수를 완성해보세요.
 */
+func solution19(_ common:[Int]) -> Int {
+    if common[1]-common[0] == common[2]-common[1] {
+        return common[1]-common[0]+(common.last ?? 0)
+    } else {
+        return (common[1]/common[0])*(common.last ?? 0)
+    }
+}
 
 /* 연속된 수의 합
 
