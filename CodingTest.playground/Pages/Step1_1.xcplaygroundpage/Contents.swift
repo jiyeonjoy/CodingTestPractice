9283abcccb9fd0fe56fac47240428342cb5c8abf
jiyeonchoi <choi02647@naver.com>
2022-11-25 21:07:36 +0900

    문자열 내 마음대로 정렬하기

--- CodingTest.playground/Pages/Step1_1.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step1_1.xcplaygroundpage/Contents.swift
@@ -68,10 +68,42 @@ import Foundation
 
  -
 */
-/*
 
- -
+/* 문자열 내 마음대로 정렬하기
+
+ - 문자열로 구성된 리스트 strings와, 정수 n이 주어졌을 때, 각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬하려 합니다. 예를 들어 strings가 ["sun", "bed", "car"]이고 n이 1이면 각 단어의 인덱스 1의 문자 "u", "e", "a"로 strings를 정렬합니다.
+ 
+ 제한 조건
+ strings는 길이 1 이상, 50이하인 배열입니다.
+ strings의 원소는 소문자 알파벳으로 이루어져 있습니다.
+ strings의 원소는 길이 1 이상, 100이하인 문자열입니다.
+ 모든 strings의 원소의 길이는 n보다 큽니다.
+ 인덱스 1의 문자가 같은 문자열이 여럿 일 경우, 사전순으로 앞선 문자열이 앞쪽에 위치합니다.
 */
+func solution6(_ strings:[String], _ n:Int) -> [String] {
+    return strings.sorted{  Array($0)[n] == Array($1)[n] ? $0 < $1 :  Array($0)[n] < Array($1)[n] }
+}
+
+func solution5(_ strings:[String], _ n:Int) -> [String] {
+    let list:[String] = strings.sorted()
+    var dic:[Character:[String]] = [:]
+    var result:[String] = []
+    list.map {
+        if dic[Array($0)[n]]?.isEmpty ?? true {
+            dic[Array($0)[n]] = [$0]
+        } else {
+            var value:[String] = dic[Array($0)[n]]!
+            value.append($0)
+            dic[Array($0)[n]] = value
+        }
+    }
+    dic.keys.sorted().map {
+        result.append(contentsOf: dic[$0]!)
+    }
+    return result
+}
+
+solution5(["sun", "bed", "car"], 1)
 
 /* 두 정수 사이의 합
 
