d81d3ff05ae091ee4c1a963942e9a1bc748fd01d
jiyeonchoi <choi02647@naver.com>
2022-11-25 23:22:37 +0900

    정수 제곱근 판별

--- CodingTest.playground/Pages/Step1_1.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step1_1.xcplaygroundpage/Contents.swift
@@ -12,10 +12,30 @@ import Foundation
 
  -
 */
-/*
 
- -
+/* 정수 제곱근 판별
+
+ - 임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
+ n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
+
+ 제한 사항
+ n은 1이상, 50000000000000 이하인 양의 정수입니다.
 */
+func solution21(_ n:Int64) -> Int64 {
+    if n == 1 {
+        return 4
+    }
+    var result:Int64 = 1
+    var value:Int64 = 1
+    while value < n {
+        result += 1
+        value = result*result
+        if value == n {
+            return (result+1)*(result+1)
+        }
+    }
+    return -1
+}
 
 /* 정수 내림차순으로 배치하기
 
