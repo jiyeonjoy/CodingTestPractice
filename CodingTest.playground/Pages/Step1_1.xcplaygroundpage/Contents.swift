57cfe0be132bb4d59180dc39b205dcb720b1910b
jiyeonchoi <choi02647@naver.com>
2022-11-25 22:47:19 +0900

    시저 암호

--- CodingTest.playground/Pages/Step1_1.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step1_1.xcplaygroundpage/Contents.swift
@@ -36,10 +36,52 @@ import Foundation
 
  -
 */
-/*
 
- -
+/* 시저 암호
+
+ - 어떤 문장의 각 알파벳을 일정한 거리만큼 밀어서 다른 알파벳으로 바꾸는 암호화 방식을 시저 암호라고 합니다. 예를 들어 "AB"는 1만큼 밀면 "BC"가 되고, 3만큼 밀면 "DE"가 됩니다. "z"는 1만큼 밀면 "a"가 됩니다. 문자열 s와 거리 n을 입력받아 s를 n만큼 민 암호문을 만드는 함수, solution을 완성해 보세요.
+ 
+ 제한 조건
+ 공백은 아무리 밀어도 공백입니다.
+ s는 알파벳 소문자, 대문자, 공백으로만 이루어져 있습니다.
+ s의 길이는 8000이하입니다.
+ n은 1 이상, 25이하인 자연수입니다.
 */
+func solution15(_ s:String, _ n:Int) -> String {
+    return s.utf8.map {
+        var code = Int($0)
+        switch code {
+            case 65...90:
+                code = (code + n - 65) % 26 + 65
+            case 97...122:
+                code = (code + n - 97) % 26 + 97
+            default:
+                break
+        }
+        return String(UnicodeScalar(code)!)
+    }.joined()
+}
+
+func solution14(_ s:String, _ n:Int) -> String {
+    let uppercase:[Character] = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    let lowercase:[Character] = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    
+    var result:String = ""
+    for i in s {
+        if i == " " {
+            result += " "
+        } else {
+            if uppercase.contains(i) {
+                result += String(uppercase[(uppercase.firstIndex(of: i)!+n)%uppercase.count])
+            } else {
+                result += String(lowercase[(lowercase.firstIndex(of: i)!+n)%lowercase.count])
+            }
+        }
+    }
+    return result
+}
+
+solution14("AB",1)
 
 /* 문자열을 정수로 바꾸기
 
