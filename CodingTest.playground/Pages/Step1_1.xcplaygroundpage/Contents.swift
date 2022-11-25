e008fa50404445e8ec8f26f06e4106b75a9edbf8
jiyeonchoi <choi02647@naver.com>
2022-11-25 23:33:36 +0900

    짝수와 홀수

--- CodingTest.playground/Pages/Step1_1.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step1_1.xcplaygroundpage/Contents.swift
@@ -4,10 +4,18 @@ import Foundation
 
  -
 */
-/*
 
- -
+/* 짝수와 홀수
+
+ - 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
+ 
+ 제한 조건
+ num은 int 범위의 정수입니다.
+ 0은 짝수입니다.
 */
+func solution23(_ num:Int) -> String {
+    num % 2 == 0 ? "Even" : "Odd"
+}
 
 /* 제일 작은 수 제거하기
 
