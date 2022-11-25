27e290d1cb3425472710cdc8434d5a64bdc92379
jiyeonchoi <choi02647@naver.com>
2022-11-25 23:02:10 +0900

    이상한 문자 만들기

--- CodingTest.playground/Pages/Step1_1.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step1_1.xcplaygroundpage/Contents.swift
@@ -28,10 +28,32 @@ import Foundation
 
  -
 */
-/*
 
- -
+/* 이상한 문자 만들기
+
+ - 문자열 s는 한 개 이상의 단어로 구성되어 있습니다. 각 단어는 하나 이상의 공백문자로 구분되어 있습니다. 각 단어의 짝수번째 알파벳은 대문자로, 홀수번째 알파벳은 소문자로 바꾼 문자열을 리턴하는 함수, solution을 완성하세요.
+ 
+ 제한 사항
+ 문자열 전체의 짝/홀수 인덱스가 아니라, 단어(공백을 기준)별로 짝/홀수 인덱스를 판단해야합니다.
+ 첫 번째 글자는 0번째 인덱스로 보아 짝수번째 알파벳으로 처리해야 합니다.
 */
+func solution17(_ s:String) -> String {
+    var isOdd = true
+    return s.map {
+        var str:String = ""
+        if $0 == " " {
+            isOdd = true
+            str = " "
+        } else if isOdd {
+            isOdd = false
+            str = $0.uppercased()
+        } else {
+            isOdd = true
+            str = $0.lowercased()
+        }
+        return str
+     }.joined()
+}
 
 /* 약수의 합
 
