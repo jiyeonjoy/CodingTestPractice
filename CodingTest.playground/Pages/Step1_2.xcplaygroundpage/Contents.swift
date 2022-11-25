a5bc8cb303c37d5e7ce62b461fa06c7646a9446f
jiyeonchoi <choi02647@naver.com>
2022-11-26 00:35:36 +0900

    직사각형 별찍기

--- CodingTest.playground/Pages/Step1_2.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step1_2.xcplaygroundpage/Contents.swift
@@ -52,10 +52,18 @@ import Foundation
  
  -
 */
-/*
+
+/* 직사각형 별찍기
  
- -
+ - 이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다.
+ 별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
 */
+let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+let (a, b) = (n[0], n[1])
+
+for i in 1...b {
+    print(String(repeating: "*", count: a))
+}
 
 /* x만큼 간격이 있는 n개의 숫자
  
