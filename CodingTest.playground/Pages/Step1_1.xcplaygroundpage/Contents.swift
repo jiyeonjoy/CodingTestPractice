f2fe3f3f4e56dee486ac74b51e96c2485be758b2
jiyeonchoi <choi02647@naver.com>
2022-11-25 23:31:46 +0900

    제일 작은 수 제거하기

--- CodingTest.playground/Pages/Step1_1.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step1_1.xcplaygroundpage/Contents.swift
@@ -8,10 +8,19 @@ import Foundation
 
  -
 */
-/*
 
- -
+/* 제일 작은 수 제거하기
+
+ - 정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수, solution을 완성해주세요. 단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요. 예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10]면 [-1]을 리턴 합니다.
 */
+func solution22(_ arr:[Int]) -> [Int] {
+    if arr.count == 1 {
+        return [-1]
+    }
+    var result = arr
+    result.remove(at: arr.firstIndex(of: arr.sorted()[0]) ?? 0)
+    return result
+}
 
 /* 정수 제곱근 판별
 
