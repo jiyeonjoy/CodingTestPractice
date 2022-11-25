bc85305f1b8d220aa8684b3f3a8564858e9cbd68
jiyeonchoi <choi02647@naver.com>
2022-11-26 00:00:20 +0900

    핸드폰 번호 가리기

--- CodingTest.playground/Pages/Step1_2.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step1_2.xcplaygroundpage/Contents.swift
@@ -64,10 +64,23 @@ import Foundation
  
  -
 */
-/*
+
+/* 핸드폰 번호 가리기
  
- -
+ - 프로그래머스 모바일은 개인정보 보호를 위해 고지서를 보낼 때 고객들의 전화번호의 일부를 가립니다.
+ 전화번호가 문자열 phone_number로 주어졌을 때, 전화번호의 뒷 4자리를 제외한 나머지 숫자를 전부 *으로 가린 문자열을 리턴하는 함수, solution을 완성해주세요.
+
+ 제한 조건
+ phone_number는 길이 4 이상, 20이하인 문자열입니다.
 */
+func solution5(_ phone_number:String) -> String {
+    return String("\(String(repeating: "*", count: phone_number.count - 4))\(phone_number.suffix(4))")
+}
+
+func solution4(_ phone_number:String) -> String {
+    String(repeating: "*", count: phone_number.count-4) +
+    String(phone_number[String.Index(encodedOffset: phone_number.count-4)...String.Index(encodedOffset: phone_number.count-1)])
+}
 
 /* 하샤드 수
  
