ae5eb3c052459545cbef49fffe9a099da085685f
jiyeonchoi <choi02647@naver.com>
2022-11-26 21:56:57 +0900

    약수의 개수와 덧셈

--- CodingTest.playground/Pages/Step1_3.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step1_3.xcplaygroundpage/Contents.swift
@@ -85,10 +85,24 @@ import Foundation
  -
 */
 
-/*
+/* 약수의 개수와 덧셈
  
- -
+ - 두 정수 left와 right가 매개변수로 주어집니다. left부터 right까지의 모든 수들 중에서, 약수의 개수가 짝수인 수는 더하고, 약수의 개수가 홀수인 수는 뺀 수를 return 하도록 solution 함수를 완성해주세요.
+
+ 제한사항
+ 1 ≤ left ≤ right ≤ 1,000
 */
+func solution4(_ left:Int, _ right:Int) -> Int {
+    (left...right).map {
+        if sqrt(Double($0)) == Double(Int(sqrt(Double($0)))) {
+            return -$0
+        } else {
+            return $0
+        }
+    }.reduce(0,+)
+}
+
+solution4(13,17)
 
 /* 로또의 최고 순위와 최저 순위
  
