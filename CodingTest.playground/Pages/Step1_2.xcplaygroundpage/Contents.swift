1f302644517d8b1dd53e2ff84bba22ab2903a91d
jiyeonchoi <choi02647@naver.com>
2022-11-26 20:58:00 +0900

    3진법 뒤집기

--- CodingTest.playground/Pages/Step1_2.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step1_2.xcplaygroundpage/Contents.swift
@@ -4,10 +4,19 @@ import Foundation
  
  -
 */
-/*
+
+/* 3진법 뒤집기
  
- -
+ - 자연수 n이 매개변수로 주어집니다. n을 3진법 상에서 앞뒤로 뒤집은 후, 이를 다시 10진법으로 표현한 수를 return 하도록 solution 함수를 완성해주세요.
+ 
+ 제한사항
+ n은 1 이상 100,000,000 이하인 자연수입니다.
 */
+func solution19(_ n:Int) -> Int {
+    Int(String(Array(String(n, radix: 3)).reversed()), radix: 3) ?? 0
+}
+
+solution19(45)
 
 /* 두 개 뽑아서 더하기
  
