70ca6b8644dad571619155e0e5ae83065636fc74
jiyeonchoi <choi02647@naver.com>
2022-11-26 22:16:07 +0900

    없는 숫자 더하기

--- CodingTest.playground/Pages/Step1_3.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step1_3.xcplaygroundpage/Contents.swift
@@ -70,10 +70,20 @@ import Foundation
  -
 */
 
-/*
+/* 없는 숫자 더하기
  
- -
+ - 0부터 9까지의 숫자 중 일부가 들어있는 정수 배열 numbers가 매개변수로 주어집니다. numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
+ 
+ 제한사항
+ 1 ≤ numbers의 길이 ≤ 9
+ 0 ≤ numbers의 모든 원소 ≤ 9
+ numbers의 모든 원소는 서로 다릅니다.
 */
+func solution7(_ numbers:[Int]) -> Int {
+    (1...9).filter {
+        !numbers.contains($0)
+    }.reduce(0,+)
+}
 
 /* 부족한 금액 계산하기
  
