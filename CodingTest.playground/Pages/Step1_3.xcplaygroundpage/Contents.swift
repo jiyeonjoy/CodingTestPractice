103d7221d999e8cbe4096a62ca69ead6df3f28a4
jiyeonchoi <choi02647@naver.com>
2022-11-27 23:44:44 +0900

    숫자 짝꿍

--- CodingTest.playground/Pages/Step1_3.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step1_3.xcplaygroundpage/Contents.swift
@@ -45,10 +45,73 @@ import Foundation
  -
 */
 
-/*
+/* 숫자 짝꿍
  
- -
+ - 두 정수 X, Y의 임의의 자리에서 공통으로 나타나는 정수 k(0 ≤ k ≤ 9)들을 이용하여 만들 수 있는 가장 큰 정수를 두 수의 짝꿍이라 합니다(단, 공통으로 나타나는 정수 중 서로 짝지을 수 있는 숫자만 사용합니다). X, Y의 짝꿍이 존재하지 않으면, 짝꿍은 -1입니다. X, Y의 짝꿍이 0으로만 구성되어 있다면, 짝꿍은 0입니다.
+ 
+ 예를 들어, X = 3403이고 Y = 13203이라면, X와 Y의 짝꿍은 X와 Y에서 공통으로 나타나는 3, 0, 3으로 만들 수 있는 가장 큰 정수인 330입니다. 다른 예시로 X = 5525이고 Y = 1255이면 X와 Y의 짝꿍은 X와 Y에서 공통으로 나타나는 2, 5, 5로 만들 수 있는 가장 큰 정수인 552입니다(X에는 5가 3개, Y에는 5가 2개 나타나므로 남는 5 한 개는 짝 지을 수 없습니다.)
+ 두 정수 X, Y가 주어졌을 때, X, Y의 짝꿍을 return하는 solution 함수를 완성해주세요.
+
+ 제한사항
+ 3 ≤ X, Y의 길이(자릿수) ≤ 3,000,000입니다.
+ X, Y는 0으로 시작하지 않습니다.
+ X, Y의 짝꿍은 상당히 큰 정수일 수 있으므로, 문자열로 반환합니다.
 */
+/// 시간 초과!! ERROR
+func solution13(_ X:String, _ Y:String) -> String {
+    var result = ""
+    if X.count <= Y.count {
+        var yList:[Character] = Array(Y)
+        X.map {
+            if yList.contains($0) {
+                yList.remove(at: yList.firstIndex(of: $0) ?? 0)
+                result += String($0)
+            }
+        }
+    } else {
+        var xList:[Character] = Array(X)
+        Y.map {
+            if xList.contains($0) {
+                xList.remove(at: xList.firstIndex(of: $0) ?? 0)
+                result += String($0)
+            }
+        }
+    }
+   
+    if result == "" {
+        result = "-1"
+    } else {
+        result = String(result.sorted(by: >))
+        if result.first == "0" {
+            result = "0"
+        }
+    }
+    return result
+}
+
+// 성공!
+func solution12(_ X:String, _ Y:String) -> String {
+    var dicX = Dictionary(grouping: X) { $0 }
+    var dicY = Dictionary(grouping: Y) { $0 }
+    var result:String = ""
+    for i in 0...9 {
+        let key:Character = Character("\(9-i)")
+        result += String(repeating: key, count: min(dicX[key]?.count ?? 0, dicY[key]?.count ?? 0))
+    }
+    if result == "" {
+        result = "-1"
+    } else {
+        if result.first == "0" {
+            result = "0"
+        }
+    }
+    return result
+}
+
+solution12("100","2345") // -1
+solution12("100","203045") // 0
+solution12("100","123450") // 10
+solution12("12321","42531") // 321
 
 /* 성격 유형 검사하기
  
