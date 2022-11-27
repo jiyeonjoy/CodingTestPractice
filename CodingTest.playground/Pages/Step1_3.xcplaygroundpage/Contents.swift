5a77f3bcfcc0fb91d32a9bb4d71bb5c6771ecea7
jiyeonchoi <choi02647@naver.com>
2022-11-28 00:41:21 +0900

    옹알이 (2)

--- CodingTest.playground/Pages/Step1_3.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step1_3.xcplaygroundpage/Contents.swift
@@ -30,10 +30,27 @@ import Foundation
  -
 */
 
-/*
+/* 옹알이 (2)
  
- -
+ - 머쓱이는 태어난 지 11개월 된 조카를 돌보고 있습니다. 조카는 아직 "aya", "ye", "woo", "ma" 네 가지 발음과 네 가지 발음을 조합해서 만들 수 있는 발음밖에 하지 못하고 연속해서 같은 발음을 하는 것을 어려워합니다. 문자열 배열 babbling이 매개변수로 주어질 때, 머쓱이의 조카가 발음할 수 있는 단어의 개수를 return하도록 solution 함수를 완성해주세요.
+ 
+ 제한사항
+ 1 ≤ babbling의 길이 ≤ 100
+ 1 ≤ babbling[i]의 길이 ≤ 30
+ 문자열은 알파벳 소문자로만 이루어져 있습니다.
 */
+func solution16(_ babbling:[String]) -> Int {
+    babbling.filter{
+        !$0.contains("ayaaya") &&
+        !$0.contains("yeye") &&
+        !$0.contains("woowoo") &&
+        !$0.contains("mama") &&
+        $0.replacingOccurrences(of: "aya", with: ".").replacingOccurrences(of: "ye", with: ".").replacingOccurrences(of: "woo", with: ".").replacingOccurrences(of: "ma", with: ".").replacingOccurrences(of: ".", with: "") == ""
+    }.count
+}
+
+solution16(["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"])
+solution16(["aya", "yee", "u", "maa"])
 
 /* 콜라 문제
  
