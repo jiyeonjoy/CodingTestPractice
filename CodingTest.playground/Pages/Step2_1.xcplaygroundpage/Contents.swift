a4224dd36009b1a081638aad35841226e730f40f
jiyeonchoi <choi02647@naver.com>
2022-11-30 00:12:47 +0900

    JadenCase 문자열 만들기

--- CodingTest.playground/Pages/Step2_1.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step2_1.xcplaygroundpage/Contents.swift
@@ -45,10 +45,39 @@ import Foundation
  -
 */
 
-/*
+/* JadenCase 문자열 만들기
 
- -
+ - JadenCase란 모든 단어의 첫 문자가 대문자이고, 그 외의 알파벳은 소문자인 문자열입니다. 단, 첫 문자가 알파벳이 아닐 때에는 이어지는 알파벳은 소문자로 쓰면 됩니다. (첫 번째 입출력 예 참고)
+ 문자열 s가 주어졌을 때, s를 JadenCase로 바꾼 문자열을 리턴하는 함수, solution을 완성해주세요.
+
+ 제한 조건
+ s는 길이 1 이상 200 이하인 문자열입니다.
+ s는 알파벳과 숫자, 공백문자(" ")로 이루어져 있습니다.
+ 숫자는 단어의 첫 문자로만 나옵니다.
+ 숫자로만 이루어진 단어는 없습니다.
+ 공백문자가 연속해서 나올 수 있습니다.
 */
+func solution11(_ s:String) -> String {
+    if s.count == 1 {
+        return s.uppercased()
+    }
+    let list:[String] = s.components(separatedBy: " ")
+    var result:String = ""
+    for i in 0...list.count-1 {
+        if list[i].count == 0 {
+            
+        } else if list[i].count == 1 {
+            result += list[i].uppercased()
+        } else {
+            var strList:[Character] = Array(list[i])
+        result += String(strList.removeFirst()).uppercased() + String(strList).lowercased()
+        }
+        result += (i == list.count-1 ? "" : " ")
+    }
+    return result
+}
+
+solution11("3people unFollowed me")
 
 /* 행렬의 곱셈
 
