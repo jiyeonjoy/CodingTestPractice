03e4f3c8b071b50ed66e6b2b7b89ce1e58f4c7a3
jiyeonchoi <choi02647@naver.com>
2022-12-02 00:00:37 +0900

    n^2 배열 자르기

--- CodingTest.playground/Pages/Step2_4.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step2_4.xcplaygroundpage/Contents.swift
@@ -65,10 +65,46 @@ import Foundation
  -
 */
 
-/*
+/* n^2 배열 자르기
 
- -
+ - 정수 n, left, right가 주어집니다. 다음 과정을 거쳐서 1차원 배열을 만들고자 합니다.
+ 
+ n행 n열 크기의 비어있는 2차원 배열을 만듭니다.
+ i = 1, 2, 3, ..., n에 대해서, 다음 과정을 반복합니다.
+ 1행 1열부터 i행 i열까지의 영역 내의 모든 빈 칸을 숫자 i로 채웁니다.
+ 1행, 2행, ..., n행을 잘라내어 모두 이어붙인 새로운 1차원 배열을 만듭니다.
+ 새로운 1차원 배열을 arr이라 할 때, arr[left], arr[left+1], ..., arr[right]만 남기고 나머지는 지웁니다.
+ 정수 n, left, right가 매개변수로 주어집니다. 주어진 과정대로 만들어진 1차원 배열을 return 하도록 solution 함수를 완성해주세요.
+
+ 제한사항
+ 1 ≤ n ≤ 107
+ 0 ≤ left ≤ right < n2
+ right - left < 105
 */
+func solution7(_ n:Int, _ left:Int64, _ right:Int64) -> [Int] {
+    var list:[Int] = []
+    var count:Int = Int(left)/n*n
+    var isDone:Bool = false
+    for i in (Int(left)/n)+1...n {
+        for j in 1...n {
+            count += 1
+            if count > left {
+                list.append(j < i ? i : j)
+                if count > right {
+                    isDone = true
+                    break
+                }
+            }
+        }
+        if isDone {
+            break
+        }
+    }
+    return list
+}
+
+solution7(4, 7, 14)
+solution7(3,2,5)
 
 /* 교점에 별 만들기
  
