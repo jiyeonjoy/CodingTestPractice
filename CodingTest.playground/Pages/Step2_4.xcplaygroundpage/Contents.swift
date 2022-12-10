3c95bd510db23492f64e66f81c99b6358f7035d3
jiyeonchoi <choi02647@naver.com>
2022-12-10 17:48:15 +0900

    연속 부분 수열 합의 개수

--- CodingTest.playground/Pages/Step2_4.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step2_4.xcplaygroundpage/Contents.swift
@@ -374,8 +374,102 @@ func solution171(_ topping:[Int]) -> Int {
 
 /* 연속 부분 수열 합의 개수
 
- -
+ - 철호는 수열을 가지고 놀기 좋아합니다. 어느 날 철호는 어떤 자연수로 이루어진 원형 수열의 연속하는 부분 수열의 합으로 만들 수 있는 수가 모두 몇 가지인지 알아보고 싶어졌습니다. 원형 수열이란 일반적인 수열에서 처음과 끝이 연결된 형태의 수열을 말합니다. 예를 들어 수열 [7, 9, 1, 1, 4] 로 원형 수열을 만들면 다음과 같습니다.
+ 
+ 원형 수열은 처음과 끝이 연결되어 끊기는 부분이 없기 때문에 연속하는 부분 수열도 일반적인 수열보다 많아집니다.
+ 원형 수열의 모든 원소 elements가 순서대로 주어질 때, 원형 수열의 연속 부분 수열 합으로 만들 수 있는 수의 개수를 return 하도록 solution 함수를 완성해주세요.
+
+ 제한사항
+ 3 ≤ elements의 길이 ≤ 1,000
+ 1 ≤ elements의 원소 ≤ 1,000
 */
+func solution15(_ elements:[Int]) -> Int {
+    var sumList:[Int] = elements
+    var list:[Int] = elements+elements
+    for i in 2...elements.count-1 {
+        var value = list[0...i-1].reduce(0,+)
+        sumList.append(value)
+        for j in 1...elements.count-1 {
+            value = value-list[j-1]+list[j+i-1]
+            sumList.append(value)
+        }
+    }
+    var result = Set<Int>()
+    result.formUnion(sumList)
+    return result.count+1
+}
+
+solution15([7,9,1,1,4])
+
+func solution153(_ elements:[Int]) -> Int {
+    var sumList:[Int] = elements
+    var list:[Int] = elements+elements
+    for i in 2...elements.count-1 {
+        for j in 0...elements.count-1 {
+            let value = list[j...j+i-1].reduce(0,+)
+            sumList.append(value)
+        }
+    }
+//    sumList.append(elements.reduce(0, +))
+    var result = Set<Int>()
+    result.formUnion(sumList)
+    return result.count+1
+}
+
+solution153([7,9,1,1,4])
+
+func solution152(_ elements:[Int]) -> Int {
+    var result = Set<Int>()
+    result.formUnion(elements)
+    var list:[Int] = elements+elements
+    for i in 2...elements.count-1 {
+        for j in 0...elements.count-1 {
+            let value = list[j...j+i-1].reduce(0,+)
+            result.insert(value)
+        }
+    }
+    result.insert(elements.reduce(0,+))
+    return result.count
+}
+
+solution152([7,9,1,1,4])
+
+func solution151(_ elements:[Int]) -> Int {
+    var sumList:[Int] = elements
+    for i in 2...elements.count {
+        var list:[Int] = elements
+        if i != 1 && i != elements.count {
+            list += elements[0...i-1]
+        }
+        for j in 0...list.count-i {
+            let value = list[j...j+i-1].reduce(0,+)
+            sumList.append(value)
+        }
+    }
+    var result = Set<Int>()
+    result.formUnion(sumList)
+    return result.count
+}
+
+solution151([7,9,1,1,4])
+
+func solution150(_ elements:[Int]) -> Int {
+    var result = Set<Int>()
+    result.formUnion(elements)
+    for i in 2...elements.count {
+        var list:[Int] = elements
+        if i != 1 && i != elements.count {
+            list += elements[0...i-1]
+        }
+        for j in 0...list.count-i {
+            let value = list[j...j+i-1].reduce(0,+)
+            result.insert(value)
+        }
+    }
+    return result.count
+}
+
+solution150([7,9,1,1,4])
 
 /* 혼자 놀기의 달인
 
