d5e875db9316b04f157c6ab3a9ab5fb1ba110412
jiyeonchoi <choi02647@naver.com>
2022-12-14 23:34:42 +0900

    기지국 설치

--- CodingTest.playground/Pages/Step3_1.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step3_1.xcplaygroundpage/Contents.swift
@@ -1,5 +1,61 @@
 import Foundation
 
+/* 기지국 설치
+ 
+ - N개의 아파트가 일렬로 쭉 늘어서 있습니다. 이 중에서 일부 아파트 옥상에는 4g 기지국이 설치되어 있습니다. 기술이 발전해 5g 수요가 높아져 4g 기지국을 5g 기지국으로 바꾸려 합니다. 그런데 5g 기지국은 4g 기지국보다 전달 범위가 좁아, 4g 기지국을 5g 기지국으로 바꾸면 어떤 아파트에는 전파가 도달하지 않습니다.
+ 
+ 예를 들어 11개의 아파트가 쭉 늘어서 있고, [4, 11] 번째 아파트 옥상에는 4g 기지국이 설치되어 있습니다. 만약 이 4g 기지국이 전파 도달 거리가 1인 5g 기지국으로 바뀔 경우 모든 아파트에 전파를 전달할 수 없습니다. (전파의 도달 거리가 W일 땐, 기지국이 설치된 아파트를 기준으로 전파를 양쪽으로 W만큼 전달할 수 있습니다.)
+
+ 초기에, 1, 2, 6, 7, 8, 9번째 아파트에는 전파가 전달되지 않습니다.
+ 기지국설치1_pvskxt.png
+
+ 1, 7, 9번째 아파트 옥상에 기지국을 설치할 경우, 모든 아파트에 전파를 전달할 수 있습니다.
+ 기지국설치2_kml0pb.png
+
+ 더 많은 아파트 옥상에 기지국을 설치하면 모든 아파트에 전파를 전달할 수 있습니다.
+ 기지국설치3_xhv7r3.png
+
+ 이때, 우리는 5g 기지국을 최소로 설치하면서 모든 아파트에 전파를 전달하려고 합니다. 위의 예시에선 최소 3개의 아파트 옥상에 기지국을 설치해야 모든 아파트에 전파를 전달할 수 있습니다.
+
+ 아파트의 개수 N, 현재 기지국이 설치된 아파트의 번호가 담긴 1차원 배열 stations, 전파의 도달 거리 W가 매개변수로 주어질 때, 모든 아파트에 전파를 전달하기 위해 증설해야 할 기지국 개수의 최솟값을 리턴하는 solution 함수를 완성해주세요
+
+ 제한사항
+ N: 200,000,000 이하의 자연수
+ stations의 크기: 10,000 이하의 자연수
+ stations는 오름차순으로 정렬되어 있고, 배열에 담긴 수는 N보다 같거나 작은 자연수입니다.
+ W: 10,000 이하의 자연수
+*/
+func getKJGCount(_ range:Int, _ v:Int) ->Int {
+    if v%range == 0 {
+        return v/range
+    } else {
+        return v/range+1
+    }
+}
+
+func solution6(_ n:Int, _ stations:[Int], _ w:Int) -> Int{
+    var startIndex:Int = 1
+    var range:Int = w+w+1
+    var result:Int = 0
+
+    for i in 0...stations.count-1 {
+        if stations[i] - w > startIndex {
+            var v = stations[i] - w - startIndex
+            var count = getKJGCount(range, v)
+            result += count
+            startIndex = max(stations[i]+w+1, startIndex+range*count)
+        } else {
+            startIndex = stations[i]+w+1
+        }
+        if i == stations.count-1 {
+            if startIndex <= n {
+                result += getKJGCount(range, n-startIndex+1)
+            }
+        }
+    }
+    return result
+}
+
 /* 베스트앨범
  
  - 스트리밍 사이트에서 장르 별로 가장 많이 재생된 노래를 두 개씩 모아 베스트 앨범을 출시하려 합니다. 노래는 고유 번호로 구분하며, 노래를 수록하는 기준은 다음과 같습니다.
