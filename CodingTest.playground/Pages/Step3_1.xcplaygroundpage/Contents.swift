40a2b5cde2bf37f99a80c3d17898db84f3517104
jiyeonchoi <choi02647@naver.com>
2022-12-22 23:12:59 +0900

    [카카오 인턴] 보석 쇼핑

--- CodingTest.playground/Pages/Step3_1.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step3_1.xcplaygroundpage/Contents.swift
@@ -1,5 +1,231 @@
 import Foundation
 
+/* [카카오 인턴] 보석 쇼핑
+
+ - [본 문제는 정확성과 효율성 테스트 각각 점수가 있는 문제입니다.]
+ 
+ 개발자 출신으로 세계 최고의 갑부가 된 어피치는 스트레스를 받을 때면 이를 풀기 위해 오프라인 매장에 쇼핑을 하러 가곤 합니다.
+ 어피치는 쇼핑을 할 때면 매장 진열대의 특정 범위의 물건들을 모두 싹쓸이 구매하는 습관이 있습니다.
+ 어느 날 스트레스를 풀기 위해 보석 매장에 쇼핑을 하러 간 어피치는 이전처럼 진열대의 특정 범위의 보석을 모두 구매하되 특별히 아래 목적을 달성하고 싶었습니다.
+ 진열된 모든 종류의 보석을 적어도 1개 이상 포함하는 가장 짧은 구간을 찾아서 구매
+
+ 예를 들어 아래 진열대는 4종류의 보석(RUBY, DIA, EMERALD, SAPPHIRE) 8개가 진열된 예시입니다.
+
+ 진열대 번호    1    2    3    4    5    6    7    8
+ 보석 이름    DIA    RUBY    RUBY    DIA    DIA    EMERALD    SAPPHIRE    DIA
+ 진열대의 3번부터 7번까지 5개의 보석을 구매하면 모든 종류의 보석을 적어도 하나 이상씩 포함하게 됩니다.
+
+ 진열대의 3, 4, 6, 7번의 보석만 구매하는 것은 중간에 특정 구간(5번)이 빠지게 되므로 어피치의 쇼핑 습관에 맞지 않습니다.
+
+ 진열대 번호 순서대로 보석들의 이름이 저장된 배열 gems가 매개변수로 주어집니다. 이때 모든 보석을 하나 이상 포함하는 가장 짧은 구간을 찾아서 return 하도록 solution 함수를 완성해주세요.
+ 가장 짧은 구간의 시작 진열대 번호와 끝 진열대 번호를 차례대로 배열에 담아서 return 하도록 하며, 만약 가장 짧은 구간이 여러 개라면 시작 진열대 번호가 가장 작은 구간을 return 합니다.
+
+ [제한사항]
+ gems 배열의 크기는 1 이상 100,000 이하입니다.
+ gems 배열의 각 원소는 진열대에 나열된 보석을 나타냅니다.
+ gems 배열에는 1번 진열대부터 진열대 번호 순서대로 보석이름이 차례대로 저장되어 있습니다.
+ gems 배열의 각 원소는 길이가 1 이상 10 이하인 알파벳 대문자로만 구성된 문자열입니다.
+*/
+func solution8(_ gems:[String]) -> [Int] {
+    let kinds = Dictionary(grouping: gems) { $0 }.keys.count
+    if kinds == gems.count {
+        return [1, gems.count]
+    }
+    var result:[Int] = []
+    
+    var savedList:[String:Int] = [:]
+    var minCount = gems.count-1
+    var firstIndex = 0
+    var firstJ: String = ""
+    for i in 0...gems.count-1 {
+        savedList[gems[i]] = i
+        if savedList.count == kinds {
+            if firstJ == "" {
+                firstJ = savedList.sorted{ $0.value < $1.value }[0].key
+                firstIndex = savedList[firstJ]!
+                result = [firstIndex+1, i+1]
+                minCount =  i - firstIndex
+            } else {
+                let first = savedList[firstJ]!
+                if firstIndex < first {
+                    firstJ = savedList.sorted{ $0.value < $1.value }[0].key
+                    firstIndex = savedList[firstJ]!
+                    if i - firstIndex < minCount {
+                        result = [firstIndex+1, i+1]
+                        minCount =  i - firstIndex
+                    }
+                }
+            }
+        }
+    }
+    if result == [] {
+        return [1, gems.count]
+    }
+    return result
+}
+
+solution8(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]) // [3,7]
+
+// 효율성 시간초과 - 2개
+func solution84(_ gems:[String]) -> [Int] {
+    let kinds = Dictionary(grouping: gems) { $0 }.keys.count
+    if kinds == gems.count {
+        return [1, gems.count]
+    }
+    var result:[Int] = []
+    
+    var savedList:[String:Int] = [:]
+    var minCount = gems.count-1
+    
+    for i in 0...gems.count-1 {
+        savedList[gems[i]] = i
+        if savedList.count == kinds {
+            let firstIndex = savedList.values.min()!
+            if i - firstIndex < minCount {
+                result = [firstIndex+1, i+1]
+                minCount =  i - firstIndex
+            }
+           
+        }
+    }
+    if result == [] {
+        return [1, gems.count]
+    }
+    return result
+}
+
+// 효율성 시간초과
+func solution83(_ gems:[String]) -> [Int] {
+    var dic:[String:Int] = [:]
+    for i in 0...gems.count-1 {
+        dic[gems[i]] = i
+    }
+    let kinds = dic.count
+    if kinds == gems.count {
+        return [1, gems.count]
+    }
+    let index:Int = dic.values.sorted(by: <)[0]
+    var result:[Int] = []
+    
+    var list:[String] = []
+    var savedList:[String:Int] = [:]
+    var minCount = gems.count
+    var startIndex = 0
+    
+    for i in 0...gems.count-1 {
+        if list.contains(gems[i]) {
+            if gems[startIndex] == gems[i] {
+                startIndex += 1
+                while true {
+                    if (savedList[gems[startIndex]] ?? 0) > startIndex {
+                        startIndex += 1
+                    } else {
+                        break
+                    }
+                }
+            } else {
+                savedList[gems[i]] = i
+            }
+        } else {
+            list.append(gems[i])
+        }
+        if index < startIndex {
+            break
+        }
+        if list.count == kinds && i - startIndex < minCount {
+            result = [startIndex+1, i+1]
+            minCount = i - startIndex
+        }
+    }
+    if result == [] {
+        return [1, gems.count]
+    }
+    return result
+}
+
+// 효율성 시간초과
+func solution82(_ gems:[String]) -> [Int] {
+    let kinds = Dictionary(grouping: gems) { $0 }.keys.count
+    if kinds == gems.count {
+        return [1, gems.count]
+    }
+    var result:[Int] = []
+    
+    var list:[String] = []
+    var savedList:[String:Int] = [:]
+    var minCount = gems.count-1
+    var startIndex = 0
+    
+    for i in 0...gems.count-1 {
+        if list.contains(gems[i]) {
+            if gems[startIndex] == gems[i] {
+                startIndex += 1
+                while true {
+                    if (savedList[gems[startIndex]] ?? 0) > startIndex {
+                        startIndex += 1
+                    } else {
+                        break
+                    }
+                }
+            } else {
+                savedList[gems[i]] = i
+            }
+        } else {
+            list.append(gems[i])
+        }
+        if list.count == kinds && i - startIndex < minCount {
+            result = [startIndex+1, i+1]
+            minCount = i - startIndex
+        }
+    }
+    if result == [] {
+        return [1, gems.count]
+    }
+    return result
+}
+
+// 효율성 시간초과
+func solution81(_ gems:[String]) -> [Int] {
+    let kinds = Dictionary(grouping: gems) { $0 }.keys.count
+    if kinds == gems.count {
+        return [1, gems.count]
+    }
+    var result:[Int] = []
+    
+    var list:[String] = []
+    var savedList:[String] = []
+    var minCount = gems.count-1
+    var startIndex = 0
+    
+    for i in 0...gems.count-1 {
+        if list.contains(gems[i]) {
+            if gems[startIndex] == gems[i] {
+                startIndex += 1
+                while true {
+                    if savedList.contains(gems[startIndex]) {
+                        savedList.remove(at: savedList.firstIndex(of: gems[startIndex]) ?? 0)
+                        startIndex += 1
+                    } else {
+                        break
+                    }
+                }
+            } else {
+                savedList.append(gems[i])
+            }
+        } else {
+            list.append(gems[i])
+        }
+        if list.count == kinds && i - startIndex < minCount {
+            result = [startIndex+1, i+1]
+            minCount = i - startIndex
+        }
+    }
+    if result == [] {
+        return [1, gems.count]
+    }
+    return result
+}
+
 /* 불량 사용자
 
  - 개발팀 내에서 이벤트 개발을 담당하고 있는 "무지"는 최근 진행된 카카오이모티콘 이벤트에 비정상적인 방법으로 당첨을 시도한 응모자들을 발견하였습니다. 이런 응모자들을 따로 모아 불량 사용자라는 이름으로 목록을 만들어서 당첨 처리 시 제외하도록 이벤트 당첨자 담당자인 "프로도" 에게 전달하려고 합니다. 이 때 개인정보 보호을 위해 사용자 아이디 중 일부 문자를 '*' 문자로 가려서 전달했습니다. 가리고자 하는 문자 하나에 '*' 문자 하나를 사용하였고 아이디 당 최소 하나 이상의 '*' 문자를 사용하였습니다.
