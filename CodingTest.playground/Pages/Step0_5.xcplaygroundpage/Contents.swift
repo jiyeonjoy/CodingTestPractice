293ed99007f008d0ede5b016d40dff9b14fede8b
최지연 <choi02647@naver.com>
2023-08-28 18:01:35 +0900

    solution21

--- CodingTest.playground/Pages/Step0_5.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step0_5.xcplaygroundpage/Contents.swift
@@ -1,5 +1,12 @@
 import Foundation
 
+func solution21(_ a: Int, _ b: Int) -> Int {
+    let n1 = Int(truncating: NSDecimalNumber(decimal: pow(10, String(b).count)*Decimal(a)+Decimal(b)))
+    let n2 = Int(truncating: NSDecimalNumber(decimal: pow(10, String(a).count)*Decimal(b)+Decimal(a)))
+    
+    return max(n1, n2)
+}
+
 /* 옹알이 (1)
 
  - 머쓱이는 태어난 지 6개월 된 조카를 돌보고 있습니다. 조카는 아직 "aya", "ye", "woo", "ma" 네 가지 발음을 최대 한 번씩 사용해 조합한(이어 붙인) 발음밖에 하지 못합니다. 문자열 배열 babbling이 매개변수로 주어질 때, 머쓱이의 조카가 발음할 수 있는 단어의 개수를 return하도록 solution 함수를 완성해주세요.
