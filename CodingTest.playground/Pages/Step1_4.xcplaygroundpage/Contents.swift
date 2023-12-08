bb814b9963fff6da44efa345c0141f0b21ab6617
최지연 <choi02647@naver.com>
2023-12-08 13:35:17 +0900

    추억 점수

--- CodingTest.playground/Pages/Step1_4.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/Step1_4.xcplaygroundpage/Contents.swift
@@ -1,5 +1,31 @@
 import Foundation
 
+/* 추억 점수
+ 
+ - 사진들을 보며 추억에 젖어 있던 루는 사진별로 추억 점수를 매길려고 합니다. 사진 속에 나오는 인물의 그리움 점수를 모두 합산한 값이 해당 사진의 추억 점수가 됩니다. 예를 들어 사진 속 인물의 이름이 ["may", "kein", "kain"]이고 각 인물의 그리움 점수가 [5점, 10점, 1점]일 때 해당 사진의 추억 점수는 16(5 + 10 + 1)점이 됩니다. 다른 사진 속 인물의 이름이 ["kali", "mari", "don", "tony"]이고 ["kali", "mari", "don"]의 그리움 점수가 각각 [11점, 1점, 55점]]이고, "tony"는 그리움 점수가 없을 때, 이 사진의 추억 점수는 3명의 그리움 점수를 합한 67(11 + 1 + 55)점입니다.
+ 
+ 그리워하는 사람의 이름을 담은 문자열 배열 name, 각 사람별 그리움 점수를 담은 정수 배열 yearning, 각 사진에 찍힌 인물의 이름을 담은 이차원 문자열 배열 photo가 매개변수로 주어질 때, 사진들의 추억 점수를 photo에 주어진 순서대로 배열에 담아 return하는 solution 함수를 완성해주세요.
+*/
+func solution9(_ name:[String], _ yearning:[Int], _ photo:[[String]]) -> [Int] {
+    var dic:[String:Int] = [:]
+    for i in 0..<name.count {
+        dic[name[i]] = yearning[i]
+    }
+    var result:[Int] = []
+    for names in photo {
+        var sum = 0
+        for str in name {
+            if names.contains(str) {
+                sum += dic[str] ?? 0
+            }
+        }
+        result.append(sum)
+    }
+    return result
+}
+
+print(solution9(["may", "kein", "kain", "radi"], [5, 10, 1, 3], [["may", "kein", "kain", "radi"],["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]])) // [19, 15, 6]
+
 /* 달리기 경주
  
  - 얀에서는 매년 달리기 경주가 열립니다. 해설진들은 선수들이 자기 바로 앞의 선수를 추월할 때 추월한 선수의 이름을 부릅니다. 예를 들어 1등부터 3등까지 "mumu", "soe", "poe" 선수들이 순서대로 달리고 있을 때, 해설진이 "soe"선수를 불렀다면 2등인 "soe" 선수가 1등인 "mumu" 선수를 추월했다는 것입니다. 즉 "soe" 선수가 1등, "mumu" 선수가 2등으로 바뀝니다.
